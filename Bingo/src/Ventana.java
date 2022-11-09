74f95f899a359be0ac9fccdbf78e02503b4f9b74
javiedu <jafezca.edu@gmail.com>
2022-11-09 12:24:47 +0100

    Los numeros se almacenan en el archivo

--- Bingo/src/Ventana.java
+++ Bingo/src/Ventana.java
@@ -4,22 +4,28 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.BorderLayout;
 import java.awt.Color;
-
 import javax.swing.JLayeredPane;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
-import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 @SuppressWarnings("serial")
 public class Ventana extends JFrame {
 
 	private JPanel contentPane;
+
+	private static JButton[] labels = new JButton[90];
+	private static int cantNumeros = 0, pos = 0;
 	private static int[] numeros = new int[90];
-	private static JLabel[] labels = new JLabel[90];
-	private static int pos = 0;
+	private static JLabel lblNumeroGrande;
+	
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -36,7 +42,7 @@ public class Ventana extends JFrame {
 
 	public Ventana() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 605, 469);
+		setBounds(100, 100, 639, 489);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -49,6 +55,11 @@ public class Ventana extends JFrame {
 		
 		JPanel panelNumeroGrande = new JPanel();
 		layeredPane.add(panelNumeroGrande, BorderLayout.EAST);
+		panelNumeroGrande.setLayout(new BorderLayout(0, 0));
+		
+		lblNumeroGrande = new JLabel("XX");
+		lblNumeroGrande.setFont(new Font("Tahoma", Font.PLAIN, 50));
+		panelNumeroGrande.add(lblNumeroGrande);
 		
 		JPanel panelOpciones = new JPanel();
 		layeredPane.add(panelOpciones, BorderLayout.SOUTH);
@@ -56,7 +67,14 @@ public class Ventana extends JFrame {
 		JButton btnGenerarNumero = new JButton("Sacar Bola");
 		btnGenerarNumero.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				generarNumeroJ();
+				if (cantNumeros < 90) {
+					generarNumeroJ();
+					cantNumeros++;
+					for (int i = 0; i <= pos; i++) {
+						System.out.print(numeros[i] + " ");
+					}
+					System.out.println();
+				}
 			}
 		});
 		panelOpciones.add(btnGenerarNumero);
@@ -66,9 +84,9 @@ public class Ventana extends JFrame {
 		panelNumeros.setLayout(new GridLayout(9, 10, 0, 0));
 		
 		for (int i = 0; i < 90; i++) {
-			JLabel lbl = new JLabel(String.valueOf((i + 1)));
-			panelNumeros.add(lbl);
-			labels[i] = lbl;
+			JButton btn = new JButton(String.valueOf((i + 1)));
+			panelNumeros.add(btn);
+			labels[i] = btn;
 		}
 	}
 	
@@ -77,12 +95,26 @@ public class Ventana extends JFrame {
 		do {
 			igual = true;
 			int num = (int)(Math.random()*90+1);
-			System.out.println(num);
 			if(labels[num - 1].getBackground() != Color.GREEN) {
-				System.out.println("Entra");
 				labels[num - 1].setBackground(Color.GREEN);
 				igual = false;
+				numeros[pos] = num;
+				pos++;
+				if (num > 9) {
+					lblNumeroGrande.setText(String.valueOf(num));
+				} else {
+					lblNumeroGrande.setText(" " + String.valueOf(num));
+				}
+				try {
+					PrintWriter fichero = new PrintWriter(new File("fichero"));
+					for (int i = 0; i < pos; i++) {
+						fichero.println(numeros[i]);
+					}
+					fichero.close();
+				} catch (FileNotFoundException e) {
 					
+				}
+				
 			}
 
 		} while (igual);
