31fd1ff899184d9610aa8b2c4ae6ea9bf619501f
ander-amq <dambelbrossl@gmail.com>
2022-11-09 08:43:44 +0100

    Diseño caton

--- Bingo/src/Carton.java
+++ Bingo/src/Carton.java
@@ -10,6 +10,19 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
+import java.awt.CardLayout;
+import javax.swing.BoxLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import net.miginfocom.swing.MigLayout;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.SpringLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 @SuppressWarnings("serial")
 public class Carton extends JFrame {
@@ -40,7 +53,8 @@ public class Carton extends JFrame {
 
 	public Carton() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 556, 300);
+		setBounds(100, 100, 567, 314);
+		setResizable(false);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -51,7 +65,7 @@ public class Carton extends JFrame {
 		contentPane.add(layeredPane, BorderLayout.CENTER);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 530, 128);
+		panel.setBounds(0, 43, 541, 160);
 		layeredPane.add(panel);
 		panel.setLayout(new GridLayout(3, 9, 0, 0));
 		
@@ -163,6 +177,27 @@ public class Carton extends JFrame {
 		panel.add(btnV3H9);
 		carton[8][2] = btnV3H9;
 		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(0, 214, 541, 51);
+		layeredPane.add(panel_1);
+		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JButton btnNewButton = new JButton("LINEA");
+		panel_1.add(btnNewButton);
+		
+		JButton btnBingo = new JButton("BINGO");
+		panel_1.add(btnBingo);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(0, 0, 531, 43);
+		layeredPane.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("BINGOBROS");
+		lblNewLabel.setFont(new Font("Snap ITC", Font.PLAIN, 35));
+		lblNewLabel.setBounds(145, 0, 279, 43);
+		panel_2.add(lblNewLabel);
+		
 		generarEspaciosVaciosJ();
 		generarNumerosColumnasJ();
 		generarListener();
@@ -243,5 +278,4 @@ public class Carton extends JFrame {
 		    return false;  
 		}  
 	}
-
 }
