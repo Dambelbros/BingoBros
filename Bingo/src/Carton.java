9c26460616a33a4bfa32aefff3f9e7bb7764a85a
javiedu <jafezca.edu@gmail.com>
2022-11-10 14:16:35 +0100

    j

--- Bingo/src/Carton.java
+++ Bingo/src/Carton.java
@@ -29,7 +29,7 @@ public class Carton extends JFrame {
 	private JPanel contentPane;
 	private static int[][] numeros = new int[COL][FIL];
 	private static JButton[][] carton = new JButton[COL][FIL];
-	private static JButton btnLinea;
+	private static JButton btnLinea, btnBingo;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -43,6 +43,7 @@ public class Carton extends JFrame {
 			}
 		});
 
+		/*Control de daños: Cambiar 120 por 0*/
 		for (int j = 0; j < FIL; j++) {
 			for (int i = 0; i < COL; i++) {
 				numeros[i][j] = 120;
@@ -64,166 +65,41 @@ public class Carton extends JFrame {
 		JLayeredPane layeredPane = new JLayeredPane();
 		contentPane.add(layeredPane, BorderLayout.CENTER);
 
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 43, 541, 160);
-		layeredPane.add(panel);
-		panel.setLayout(new GridLayout(3, 9, 0, 0));
+		JPanel panelCasillas = new JPanel();
+		panelCasillas.setBounds(0, 43, 541, 160);
+		layeredPane.add(panelCasillas);
+		panelCasillas.setLayout(new GridLayout(3, 9, 0, 0));
+		
+		/*Genera los botones de las casillas*/
+		for (int i = 0; i < FIL; i++) {
+			for (int j = 0; j < COL; j++) {
+				JButton btnCasilla = new JButton("");
+				btnCasilla.setBackground(Color.WHITE);
+				panelCasillas.add(btnCasilla);
+				carton[j][i] = btnCasilla;
+			}
+		}
 
-		JButton btnV1H1 = new JButton("");
-		btnV1H1.setBackground(Color.WHITE);
-		panel.add(btnV1H1);
-		carton[0][0] = btnV1H1;
-
-		JButton btnV1H2 = new JButton("");
-		btnV1H2.setBackground(Color.WHITE);
-		panel.add(btnV1H2);
-		carton[1][0] = btnV1H2;
-
-		JButton btnV1H3 = new JButton("");
-		btnV1H3.setBackground(Color.WHITE);
-		panel.add(btnV1H3);
-		carton[2][0] = btnV1H3;
-
-		JButton btnV1H4 = new JButton("");
-		btnV1H4.setBackground(Color.WHITE);
-		panel.add(btnV1H4);
-		carton[3][0] = btnV1H4;
-
-		JButton btnV1H5 = new JButton("");
-		btnV1H5.setBackground(Color.WHITE);
-		panel.add(btnV1H5);
-		carton[4][0] = btnV1H5;
-
-		JButton btnV1H6 = new JButton("");
-		btnV1H6.setBackground(Color.WHITE);
-		panel.add(btnV1H6);
-		carton[5][0] = btnV1H6;
-
-		JButton btnV1H7 = new JButton("");
-		btnV1H7.setBackground(Color.WHITE);
-		panel.add(btnV1H7);
-		carton[6][0] = btnV1H7;
-
-		JButton btnV1H8 = new JButton("");
-		btnV1H8.setBackground(Color.WHITE);
-		panel.add(btnV1H8);
-		carton[7][0] = btnV1H8;
-
-		JButton btnV1H9 = new JButton("");
-		btnV1H9.setBackground(Color.WHITE);
-		panel.add(btnV1H9);
-		carton[8][0] = btnV1H9;
-
-		JButton btnV2H1 = new JButton("");
-		btnV2H1.setBackground(Color.WHITE);
-		panel.add(btnV2H1);
-		carton[0][1] = btnV2H1;
-
-		JButton btnV2H2 = new JButton("");
-		btnV2H2.setBackground(Color.WHITE);
-		panel.add(btnV2H2);
-		carton[1][1] = btnV2H2;
-
-		JButton btnV2H3 = new JButton("");
-		btnV2H3.setBackground(Color.WHITE);
-		panel.add(btnV2H3);
-		carton[2][1] = btnV2H3;
-
-		JButton btnV2H4 = new JButton("");
-		btnV2H4.setBackground(Color.WHITE);
-		panel.add(btnV2H4);
-		carton[3][1] = btnV2H4;
-
-		JButton btnV2H5 = new JButton("");
-		btnV2H5.setBackground(Color.WHITE);
-		panel.add(btnV2H5);
-		carton[4][1] = btnV2H5;
-
-		JButton btnV2H6 = new JButton("");
-		btnV2H6.setBackground(Color.WHITE);
-		panel.add(btnV2H6);
-		carton[5][1] = btnV2H6;
-
-		JButton btnV2H7 = new JButton("");
-		btnV2H7.setBackground(Color.WHITE);
-		panel.add(btnV2H7);
-		carton[6][1] = btnV2H7;
-
-		JButton btnV2H8 = new JButton("");
-		btnV2H8.setBackground(Color.WHITE);
-		panel.add(btnV2H8);
-		carton[7][1] = btnV2H8;
-
-		JButton btnV2H9 = new JButton("");
-		btnV2H9.setBackground(Color.WHITE);
-		panel.add(btnV2H9);
-		carton[8][1] = btnV2H9;
-
-		JButton btnV3H1 = new JButton("");
-		btnV3H1.setBackground(Color.WHITE);
-		panel.add(btnV3H1);
-		carton[0][2] = btnV3H1;
-
-		JButton btnV3H2 = new JButton("");
-		btnV3H2.setBackground(Color.WHITE);
-		panel.add(btnV3H2);
-		carton[1][2] = btnV3H2;
-
-		JButton btnV3H3 = new JButton("");
-		btnV3H3.setBackground(Color.WHITE);
-		panel.add(btnV3H3);
-		carton[2][2] = btnV3H3;
-
-		JButton btnV3H4 = new JButton("");
-		btnV3H4.setBackground(Color.WHITE);
-		panel.add(btnV3H4);
-		carton[3][2] = btnV3H4;
-
-		JButton btnV3H5 = new JButton("");
-		btnV3H5.setBackground(Color.WHITE);
-		panel.add(btnV3H5);
-		carton[4][2] = btnV3H5;
-
-		JButton btnV3H6 = new JButton("");
-		btnV3H6.setBackground(Color.WHITE);
-		panel.add(btnV3H6);
-		carton[5][2] = btnV3H6;
-
-		JButton btnV3H7 = new JButton("");
-		btnV3H7.setBackground(Color.WHITE);
-		panel.add(btnV3H7);
-		carton[6][2] = btnV3H7;
-
-		JButton btnV3H8 = new JButton("");
-		btnV3H8.setBackground(Color.WHITE);
-		panel.add(btnV3H8);
-		carton[7][2] = btnV3H8;
-
-		JButton btnV3H9 = new JButton("");
-		btnV3H9.setBackground(Color.WHITE);
-		panel.add(btnV3H9);
-		carton[8][2] = btnV3H9;
-
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(0, 214, 541, 51);
-		layeredPane.add(panel_1);
-		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
+		JPanel panelBotones = new JPanel();
+		panelBotones.setBounds(0, 214, 541, 51);
+		layeredPane.add(panelBotones);
+		panelBotones.setLayout(new GridLayout(0, 2, 0, 0));
 
 		btnLinea = new JButton("LINEA");
-		panel_1.add(btnLinea);
+		panelBotones.add(btnLinea);
 
-		JButton btnBingo = new JButton("BINGO");
-		panel_1.add(btnBingo);
+		btnBingo = new JButton("BINGO");
+		panelBotones.add(btnBingo);
 
-		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(0, 0, 531, 43);
-		layeredPane.add(panel_2);
-		panel_2.setLayout(null);
+		JPanel panelArriba = new JPanel();
+		panelArriba.setBounds(0, 0, 531, 43);
+		layeredPane.add(panelArriba);
+		panelArriba.setLayout(null);
 
 		JLabel lblNewLabel = new JLabel("BINGOBROS");
 		lblNewLabel.setFont(new Font("Snap ITC", Font.PLAIN, 35));
 		lblNewLabel.setBounds(145, 0, 279, 43);
-		panel_2.add(lblNewLabel);
+		panelArriba.add(lblNewLabel);
 
 		generarEspaciosVaciosJ();
 		generarNumerosColumnasJ();
@@ -242,22 +118,33 @@ public class Carton extends JFrame {
 				comprobarLinea();
 			}
 		});
+		
+		btnBingo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				comprobarBingo();
+			}
+		});
 	}
 
 	private static void generarEspaciosVaciosJ() {
 		int[] vacio = new int[4];
 		for (int fila = 0; fila < FIL; fila++) {
-			int aux = 0;
-			int num;
+			int aux = 0, num;
 			boolean igual;
+			
 			while(aux < 4) {
 				igual = false;
 				num = (int)(Math.random()*9+0);
+				/*Comprueba que la casilla no este en la lista de casillas vacias y si es 
+				 * la ultima fila comprueba que las dos de arriba no esten en negro*/
 				for (int i = 0; i < vacio.length; i++) {
-					if (num == vacio[i]) {
+					if (num == vacio[i] || (fila == 2 && (carton[num][0].getBackground().equals(Color.BLACK) && carton[num][1].getBackground().equals(Color.BLACK)))) {
 						igual = true;
+						i = 4;
 					}
 				}
+				
+				/*Si no es igual lo añade a la lista de las casillas negras*/
 				if(!igual) {
 					vacio[aux] = num;
 					aux++;
@@ -272,17 +159,54 @@ public class Carton extends JFrame {
 	}
 
 	private static void generarNumerosColumnasJ() {
-		int min, sum;
+		int min, sum, menor, medio = 0, mayor, menorPos = 0, mayorPos = 0;
+		int[] arrayNum = new int[3];
 		for (int i = 0; i < COL; i++) {
+			/*Genera numeros aleatorios comprobando que no coincide con ninguno de los anteriores*/
 			min = i * 10;
-			for (int j = 0; j < FIL; j++) {
-				if(!carton[i][j].getBackground().equals(Color.BLACK)) {
-					do {
-						sum = (int)(Math.random()*10+1) + min;
-					} while (aNumeroJ(carton[i][0]) == sum || aNumeroJ(carton[i][1]) == sum || aNumeroJ(carton[i][2]) == sum);
-
-					carton[i][j].setText(String.valueOf(sum));	
+			arrayNum[0] = (int)(Math.random()*10+1) + min;
+			do {
+				sum = (int)(Math.random()*10+1) + min;
+			} while (arrayNum[0] == sum);
+			arrayNum[1] = sum;
+			do {
+				sum = (int)(Math.random()*10+1) + min;
+			} while (arrayNum[0] == sum || arrayNum[1] == sum);
+			arrayNum[2] = sum;
+			
+			/*Ordena los numeros de menor a mayor*/
+			mayor = arrayNum[0];
+			menor = arrayNum[0];
+			
+			for (int j = 0; j < arrayNum.length; j++) {
+				if (arrayNum[j] >= mayor) {
+					mayor = arrayNum[j];
+					mayorPos = j;
 				}
+				
+				if(arrayNum[j] <= menor) {
+					menor = arrayNum[j];
+					menorPos = j;
+				}
+			}
+			
+			for (int j = 0; j < arrayNum.length; j++) {
+				if(j != mayorPos && j != menorPos) {
+					medio = arrayNum[j];
+				}
+			}
+			
+			/*Cambia fondo y deshabilita los botones de las casillas en negro*/
+			if(carton[i][0].getBackground().equals(Color.WHITE)) {
+				carton[i][0].setText(String.valueOf(menor));
+			}
+			
+			if(carton[i][1].getBackground().equals(Color.WHITE)) {
+				carton[i][1].setText(String.valueOf(medio));
+			}
+			
+			if(carton[i][2].getBackground().equals(Color.WHITE)) {
+				carton[i][2].setText(String.valueOf(mayor));
 			}
 		}
 	}
@@ -291,10 +215,18 @@ public class Carton extends JFrame {
 		if(esNumero(boton.getText())) {
 			return Integer.parseInt(boton.getText());
 		}
-
 		return 0;
 	}
 
+	private static boolean esNumero(String str) { 
+		try {  
+			Double.parseDouble(str);  
+			return true;
+		} catch(NumberFormatException e){  
+			return false;  
+		}  
+	}
+	
 	private static void cambioColor(JButton boton) {
 		boton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -305,15 +237,6 @@ public class Carton extends JFrame {
 				}
 			}
 		});
-	}
-
-	private static boolean esNumero(String str) { 
-		try {  
-			Double.parseDouble(str);  
-			return true;
-		} catch(NumberFormatException e){  
-			return false;  
-		}  
 	}
 
 	public static void comprobarLinea() {
@@ -377,44 +300,50 @@ public class Carton extends JFrame {
 	public static void comprobarBingo() {
 		boolean correcto = true, igual;
 		try {
-			Scanner fichero = new Scanner (new File("fichero"));
-			String numero;
-			ArrayList<String> bingo = new ArrayList<String>();
-			while (fichero.hasNext()) {
-				numero = (String) fichero.next();
-				bingo.add(numero);
-			}
-			
-			for (int i = 0; i < FIL; i++) {
-				for (int j = 0; j < COL; j++) {
-					igual = false;
-					if(carton[j][i].getBackground().equals(Color.WHITE)) {
-						correcto = false;
-						break;
-					} else if(carton[j][i].getBackground().equals(Color.GREEN)) {
-						for (int numBingo = 0; numBingo < bingo.size(); numBingo++) {
-							if (Integer.parseInt(bingo.get(numBingo)) == aNumeroJ(carton[j][i])) {
-								igual = true;
-							}
-						}
-						
-						if(!igual) {
+			Scanner ganado = new Scanner (new File("ganadoBingo"));
+			ganado.close();
+			JOptionPane.showMessageDialog(null, "Ya se ha cantado bingo");
+		} catch (Exception e) {
+			try {
+				Scanner fichero = new Scanner (new File("fichero"));
+				String numero;
+				ArrayList<String> bingo = new ArrayList<String>();
+				while (fichero.hasNext()) {
+					numero = (String) fichero.next();
+					bingo.add(numero);
+				}
+
+				for (int i = 0; i < FIL; i++) {
+					for (int j = 0; j < COL; j++) {
+						igual = false;
+						if(carton[j][i].getBackground().equals(Color.WHITE)) {
 							correcto = false;
 							break;
+						} else if(carton[j][i].getBackground().equals(Color.GREEN)) {
+							for (int numBingo = 0; numBingo < bingo.size(); numBingo++) {
+								if (Integer.parseInt(bingo.get(numBingo)) == aNumeroJ(carton[j][i])) {
+									igual = true;
+								}
+							}
+
+							if(!igual) {
+								correcto = false;
+								break;
+							}
 						}
 					}
 				}
-			}
-			if(correcto) {
-				JOptionPane.showMessageDialog(null, "Has cantado bingo, has ganado");
-				PrintWriter ganado = new PrintWriter(new File("ganadoBingo"));
-				ganado.close();
-			} else {
-				JOptionPane.showMessageDialog(null, "Has cantado bingo incorrectamente");
-			}
-			
-		} catch (Exception e) {
+				if(correcto) {
+					JOptionPane.showMessageDialog(null, "Has cantado bingo, has ganado");
+					PrintWriter ganado = new PrintWriter(new File("ganadoBingo"));
+					ganado.close();
+				} else {
+					JOptionPane.showMessageDialog(null, "Has cantado bingo incorrectamente");
+				}
 
+			} catch (Exception e1) {
+
+			}
 		}
 	}
 }
